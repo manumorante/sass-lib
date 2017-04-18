2ab43c814053b075b1f1efb48e2edd3fe19b1340
Manu Morante <manuel.morante@betvictor.com>
2017-04-18 13:33:45 +0200

    Clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,9 @@
 var gulp = require('gulp');
-var del = require('del');
 var scsslint = require('gulp-scss-lint');
 
 gulp.task('scss-lint', function() {
-  gulp.src('src/_styles.scss')
+  gulp.src('_styles.scss')
     .pipe(scsslint({'config': '.scss-lint.yml'}));
 });
 
-gulp.task('sass', function () {
-  gulp.src('src/_styles.scss')
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('clean', function () {
-  del.sync('dist');
-});
-
-gulp.task('default', ['clean'], function () {
-  gulp.start('scss-lint');
-  gulp.start('sass');
-});
+gulp.task('default', ['scss-lint']);
